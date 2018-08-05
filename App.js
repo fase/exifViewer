97e54d887e26194d96d8eff83e6c2560e96e2338
Jim Fasenmyer <jfasenmyer@gmail.com>
2018-08-05 11:23:07 -0400

    Created image list with geolocation alert on select.

--- App.js
+++ App.js
@@ -1,29 +1,88 @@
+'use strict';
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
  * @format
  * @flow
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {
+  Platform, 
+  StyleSheet, 
+  Text, 
+  View, 
+  CameraRoll, 
+  Image, 
+  TouchableHighlight, 
+  ScrollView,
+  Dimensions,
+  Alert,
+} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const { width } = Dimensions.get('window');
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { photos: undefined };
+  }
+
+  getPhotos = () => {
+    CameraRoll.getPhotos({
+      first: 20,
+      assetType: 'All'
+    })
+    .then(r => this.setState({ photos: r.edges }))
+    .catch((err) => {
+      //Error Loading Images
+      console.log(err);
+   });
+  }
+
+  componentDidMount = () => 
+  {
+    this.getPhotos();
+  }
+
+  setIndex = (index) =>
+  {
+    let picked = this.state.photos[index];
+
+    Alert.alert('Photo Selected', `Lng: ${picked.node.location.longitude} Lat: ${picked.node.location.latitude}`);
+  }
+
+  render = () => {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        {this.state.photos && 
+          <ScrollView
+          contentContainerStyle={styles.scrollView}>
+          {
+            this.state.photos.map((p, i) => {
+              return (
+                <TouchableHighlight
+                  style={{opacity: i === this.state.index ? 0.5 : 1}}
+                  key={i}
+                  underlayColor='transparent'
+                  onPress={() => this.setIndex(i)}
+                >
+                  <Image
+                    style={{
+                      width: width/3,
+                      height: width/3
+                    }}
+                    source={{uri: p.node.image.uri}}
+                  />
+                </TouchableHighlight>
+              )
+            })
+          }
+        </ScrollView>
+        }
+
+        {!this.state.photos && 
+          <Text style={styles.welcome}>I didn't find photos!</Text>
+        }
       </View>
     );
   }
@@ -32,18 +91,14 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    flexDirection: 'row',
+    marginTop: 40,
+    // justifyContent: 'center',
+    // alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  scrollView: {
+    flexWrap: 'wrap',
+    flexDirection: 'row'
   },
 });
